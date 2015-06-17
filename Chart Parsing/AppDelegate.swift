467812b028189995fc5ae539168e06c9854d7c09
FoolOfShadows <foolofshadows@gmail.com>
2015-06-17 12:31:42 -0500

    Initial Commit

---
+++ Chart Parsing/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Chart Parsing
+//
+//  Created by Fool on 6/17/15.
+//  Copyright (c) 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
