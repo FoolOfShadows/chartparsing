cbe3dd920f615a909ccebe291cf77bce54a8c54a
russ whelchel <foolofshadows@gmail.com>
2018-03-15 12:23:39 -0500

    Switched the text cleaning method to use regexes and updated the bits to clean constants to improve the output

--- Chart Parsing/Constants.swift
+++ Chart Parsing/Constants.swift
@@ -8,25 +8,25 @@
 
 import Foundation
 
-let dxBadBits = ["Chronic diagnoses\n", "No active Acute diagnoses.\n", "Acute diagnoses\n", "Diagnoses", "Social history", "Term\n", "Show by", "ICD-10"]
+let dxBadBits = ["Chronic diagnoses", "No active Acute diagnoses.", "Acute diagnoses", "Diagnoses", "Social history", "Term", "Show by", "ICD-10"]
 
 let medBadBits = ["Medications", "Encounters", " Show historical"]
 
-let nutritionBadBits = ["Nutrition history\n", "Advanced directives", "Developmental history/n"]
+let nutritionBadBits = ["Nutrition history", "Advanced directives", "Developmental history"]
 
-let socialBadBits = ["Past medical history", "Social history (free text) \n", "Social history \n", "Smoking status \n", "Gender identity \n", "No gender identity recorded\n", "Sexual orientation \n", "No sexual orientation recorded\n"]
+let socialBadBits = ["Past medical history", "Social history \\(free text\\)", "Social history", "Smoking status", "Gender identity", "No gender identity recorded", "Sexual orientation", "No sexual orientation recorded"]
 
-let fmhBadBits = ["Family health history\n", "Preventive care", "Social history"]
+let fmhBadBits = ["Family health history", "Preventive care", "Social history"]
 
-let basicAllergyBadBits = ["Drug allergies\n", "Environmental allergies\n", "No environmental allergies recorded\n", "Food allergies\n", "Allergies\n", "Medications", "You have previously recorded allergies in a free-text note", "To receive interaction alerts, record the note in a structured format here. Record",  "(free-text note) Delete"]
+let basicAllergyBadBits = ["Drug allergies", "Environmental allergies", "No environmental allergies recorded", "Food allergies", "Allergies\\n", "Medications(\\s{2,}|\\n|$)", "You have previously recorded allergies in a free-text note", "To receive interaction alerts, record the note in a structured format here. Record",  "\\(free-text note\\) Delete", "Environmental Allergies: No Known Environmental Allergies"]
 
-let freeAllergyBadBits = ["Allergies (free text)\n", "ALLERGIES:", "ALLERGIES", "Use structured allergies to receive interaction alerts\n", "Food allergies:", "Food Allergies:", "Food Allergies", "Food allergies\n", "Food Allergies\n", "Environmental allergies:\n", "Environmental allergies: ", "Environmental allergies\n", "Environmental Allergies\n", "Drug allergies:", "Drug allergies-", "Drug allergies", "No Known Drug Allergies", "No Known", "Drug Allergies:", "Drug Allergies", "Family health history", "Preventive care", "No food allergies recorded\n", "Allergies\n"]
+let freeAllergyBadBits = ["Allergies \\(free text\\)", "ALLERGIES:", "ALLERGIES", "Use structured allergies to receive interaction alerts", "Food allergies:", "Food Allergies:", "Food Allergies", "Food allergies", "Food Allergies", "Environmental allergies:", "Environmental allergies: ", "Environmental allergies", "Environmental Allergies", "Drug allergies:", "Drug allergies-", "Drug allergies", "No Known Drug Allergies", "No Known", "Drug Allergies:", "Drug Allergies", "Family health history", "Preventive care", "No food allergies recorded\n", "Allergies"]
 
-let preventiveBadBits = ["Preventive care\n", "Social history", "Advance directives"]
+let preventiveBadBits = ["Preventive care", "Social history", "Advance directives"]
 
-let pshBadBits = ["Major events\n", "Ongoing medical problems", "PSH:\n", "PHS:\n"]
+let pshBadBits = ["Major events", "Ongoing medical problems", "PSH:", "PHS:"]
 
-let pmhBadBits = ["Ongoing medical problems\n", "Allergies (free text)", "Allergies\n", "PMH:\n", "PHM:\n", "Preventive care", "Family health history"]
+let pmhBadBits = ["Ongoing medical problems", "Allergies \\(free text\\)", "Allergies", "PMH:", "PHM:", "Preventive care", "Family health history"]
 
 let visitBoilerplateText = "CC:  \nProblems:  \n\n\nS:  \n\nLocation:  \nSeverity:  \nQuality:  \nDuration:  \nTiming:  \nContext:  \nModifying factors:  \nAssociated symptoms:  \n\n\nNEW PMH:  \n\n\nA(Charge):  \n\n\nP(lan):  \n\n**Rx**  \n\n\nO(PE):  \n\n\n"
 
